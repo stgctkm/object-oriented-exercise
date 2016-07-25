65827bd3312c0a12689bca5e5bb73d41e706ab91
setoguchi takuma <setoguchi.takuma@gmail.com>
2016-07-25 23:44:20 +0900

    add rule8 sample1 before refactoring

---
+++ src/example/rule8/code/sample1/befoer/Client.java
@@ -0,0 +1,33 @@
+package example.rule8.code.sample1.befoer;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+class Client {
+
+    Person person;
+
+    void howToUse() {
+        Person kent = new Person();
+        Set<Course> s = new HashSet<>();
+        s.add(new Course("Smalltalk プログラミング ", false));
+        s.add(new Course(" シングルモルトの味わい ", true));
+        kent.setCourses(s);
+
+        Course refact = new Course(" リファクタリング ", true);
+        kent.getCourses().add(refact);
+        kent.getCourses().add(new Course(" 冷酷な皮肉 ", false));
+        kent.getCourses().remove(refact);
+    }
+
+    int getNumberOfAdvancedCourses() {
+        Iterator iter = person.getCourses().iterator();
+        int count = 0;
+        while (iter.hasNext()) {
+            Course each = (Course) iter.next();
+            if (each.isAdvanced()) count++;
+        }
+        return count;
+    }
+}
