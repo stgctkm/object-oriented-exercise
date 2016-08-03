b566d7cc46b1a15eb303ea909a884f7a939c7f03
setoguchi takuma <setoguchi.takuma@gmail.com>
2016-08-03 11:57:28 +0900

    サンプル追加

---
+++ src/example/rule9/sample/FormToDto.java
@@ -0,0 +1,228 @@
+package example.rule9.sample;
+
+class FormToDto {
+
+    Dto convertFromFormToDto(Form form) {
+        Dto dto = new Dto();
+
+        dto.setFirstName(form.getFirstName());
+        dto.setLastName(form.getLastName());
+        dto.setFullName(form.getLastName() + " " + form.getFirstName());
+        dto.setAge(form.getAge());
+        dto.setCountry(form.getCountry());
+        dto.setCity(form.getCity());
+        dto.setZip(form.getZip());
+        dto.setAddressLine(form.getAddressLine());
+        dto.setAddress(form.getZip() + " " + form.getCity() + " " + form.getAddressLine());
+        dto.setGender(form.getGender());
+        dto.setHomePhone(form.getHomePhone());
+        dto.setCellPhone(form.getCellPhone());
+
+        return dto;
+    }
+
+}
+
+class Form {
+    String firstName;
+    String lastName;
+    String age;
+    String country;
+    String city;
+    String zip;
+    String addressLine;
+    String gender;
+    String homePhone;
+    String cellPhone;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getAddressLine() {
+        return addressLine;
+    }
+
+    public void setAddressLine(String addressLine) {
+        this.addressLine = addressLine;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public void setHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+    }
+
+    public String getCellPhone() {
+        return cellPhone;
+    }
+
+    public void setCellPhone(String cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+}
+
+class Dto {
+    String firstName;
+    String lastName;
+    String fullName;
+    String age;
+    String country;
+    String city;
+    String zip;
+    String addressLine;
+    String address;
+    String gender;
+    String homePhone;
+    String cellPhone;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getAddressLine() {
+        return addressLine;
+    }
+
+    public void setAddressLine(String addressLine) {
+        this.addressLine = addressLine;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public void setHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+    }
+
+    public String getCellPhone() {
+        return cellPhone;
+    }
+
+    public void setCellPhone(String cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+}
