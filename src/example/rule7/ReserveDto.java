a5c98412aa1d63c33ad448b7b531e7c27f865a99
setoguchi takuma <setoguchi.takuma@gmail.com>
2016-08-03 12:09:17 +0900

    １クラス２変数のサンプル追加

---
+++ src/example/rule7/ReserveDto.java
@@ -0,0 +1,468 @@
+package example.rule7;
+
+public class ReserveDto {
+    public String field0;
+    public String field1;
+    public String field2;
+    public String field3;
+    public String field4;
+    public String field5;
+    public String field6;
+    public String field7;
+    public String field8;
+    public String field9;
+    public String field10;
+    public String field11;
+    public String field12;
+    public String field13;
+    public String field14;
+    public String field15;
+    public String field16;
+    public String field17;
+    public String field18;
+    public String field19;
+    public String field20;
+
+    public String fieldVal0;
+    public String fieldVal1;
+    public String fieldVal2;
+    public String fieldVal3;
+    public String fieldVal4;
+    public String fieldVal5;
+    public String fieldVal6;
+    public String fieldVal7;
+    public String fieldVal8;
+    public String fieldVal9;
+    public String fieldVal10;
+    public String fieldVal11;
+    public String fieldVal12;
+    public String fieldVal13;
+    public String fieldVal14;
+    public String fieldVal15;
+    public String fieldVal16;
+    public String fieldVal17;
+    public String fieldVal18;
+    public String fieldVal19;
+    public String fieldVal20;
+
+    public String getField0() {
+        return field0;
+    }
+    public void setField0(String field0) {
+        this.field0 = field0;
+    }
+    public String getField1() {
+        return field1;
+    }
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+    public String getField2() {
+        return field2;
+    }
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+    public String getField3() {
+        return field3;
+    }
+    public void setField3(String field3) {
+        this.field3 = field3;
+    }
+    public String getField4() {
+        return field4;
+    }
+    public void setField4(String field4) {
+        this.field4 = field4;
+    }
+    public String getField5() {
+        return field5;
+    }
+    public void setField5(String field5) {
+        this.field5 = field5;
+    }
+    public String getField6() {
+        return field6;
+    }
+    public void setField6(String field6) {
+        this.field6 = field6;
+    }
+    public String getField7() {
+        return field7;
+    }
+    public void setField7(String field7) {
+        this.field7 = field7;
+    }
+    public String getField8() {
+        return field8;
+    }
+    public void setField8(String field8) {
+        this.field8 = field8;
+    }
+    public String getField9() {
+        return field9;
+    }
+    public void setField9(String field9) {
+        this.field9 = field9;
+    }
+    public String getField10() {
+        return field10;
+    }
+    public void setField10(String field10) {
+        this.field10 = field10;
+    }
+    public String getField11() {
+        return field11;
+    }
+    public void setField11(String field11) {
+        this.field11 = field11;
+    }
+    public String getField12() {
+        return field12;
+    }
+    public void setField12(String field12) {
+        this.field12 = field12;
+    }
+    public String getField13() {
+        return field13;
+    }
+    public void setField13(String field13) {
+        this.field13 = field13;
+    }
+    public String getField14() {
+        return field14;
+    }
+    public void setField14(String field14) {
+        this.field14 = field14;
+    }
+    public String getField15() {
+        return field15;
+    }
+    public void setField15(String field15) {
+        this.field15 = field15;
+    }
+    public String getField16() {
+        return field16;
+    }
+    public void setField16(String field16) {
+        this.field16 = field16;
+    }
+    public String getField17() {
+        return field17;
+    }
+    public void setField17(String field17) {
+        this.field17 = field17;
+    }
+    public String getField18() {
+        return field18;
+    }
+    public void setField18(String field18) {
+        this.field18 = field18;
+    }
+    public String getField19() {
+        return field19;
+    }
+    public void setField19(String field19) {
+        this.field19 = field19;
+    }
+    public String getField20() {
+        return field20;
+    }
+    public void setField20(String field20) {
+        this.field20 = field20;
+    }
+    public String getFieldVal0() {
+        return fieldVal0;
+    }
+    public void setFieldVal0(String fieldVal0) {
+        this.fieldVal0 = fieldVal0;
+    }
+    public String getFieldVal1() {
+        return fieldVal1;
+    }
+    public void setFieldVal1(String fieldVal1) {
+        this.fieldVal1 = fieldVal1;
+    }
+    public String getFieldVal2() {
+        return fieldVal2;
+    }
+    public void setFieldVal2(String fieldVal2) {
+        this.fieldVal2 = fieldVal2;
+    }
+    public String getFieldVal3() {
+        return fieldVal3;
+    }
+    public void setFieldVal3(String fieldVal3) {
+        this.fieldVal3 = fieldVal3;
+    }
+    public String getFieldVal4() {
+        return fieldVal4;
+    }
+    public void setFieldVal4(String fieldVal4) {
+        this.fieldVal4 = fieldVal4;
+    }
+    public String getFieldVal5() {
+        return fieldVal5;
+    }
+    public void setFieldVal5(String fieldVal5) {
+        this.fieldVal5 = fieldVal5;
+    }
+    public String getFieldVal6() {
+        return fieldVal6;
+    }
+    public void setFieldVal6(String fieldVal6) {
+        this.fieldVal6 = fieldVal6;
+    }
+    public String getFieldVal7() {
+        return fieldVal7;
+    }
+    public void setFieldVal7(String fieldVal7) {
+        this.fieldVal7 = fieldVal7;
+    }
+    public String getFieldVal8() {
+        return fieldVal8;
+    }
+    public void setFieldVal8(String fieldVal8) {
+        this.fieldVal8 = fieldVal8;
+    }
+    public String getFieldVal9() {
+        return fieldVal9;
+    }
+    public void setFieldVal9(String fieldVal9) {
+        this.fieldVal9 = fieldVal9;
+    }
+    public String getFieldVal10() {
+        return fieldVal10;
+    }
+    public void setFieldVal10(String fieldVal10) {
+        this.fieldVal10 = fieldVal10;
+    }
+    public String getFieldVal11() {
+        return fieldVal11;
+    }
+    public void setFieldVal11(String fieldVal11) {
+        this.fieldVal11 = fieldVal11;
+    }
+    public String getFieldVal12() {
+        return fieldVal12;
+    }
+    public void setFieldVal12(String fieldVal12) {
+        this.fieldVal12 = fieldVal12;
+    }
+    public String getFieldVal13() {
+        return fieldVal13;
+    }
+    public void setFieldVal13(String fieldVal13) {
+        this.fieldVal13 = fieldVal13;
+    }
+    public String getFieldVal14() {
+        return fieldVal14;
+    }
+    public void setFieldVal14(String fieldVal14) {
+        this.fieldVal14 = fieldVal14;
+    }
+    public String getFieldVal15() {
+        return fieldVal15;
+    }
+    public void setFieldVal15(String fieldVal15) {
+        this.fieldVal15 = fieldVal15;
+    }
+    public String getFieldVal16() {
+        return fieldVal16;
+    }
+    public void setFieldVal16(String fieldVal16) {
+        this.fieldVal16 = fieldVal16;
+    }
+    public String getFieldVal17() {
+        return fieldVal17;
+    }
+    public void setFieldVal17(String fieldVal17) {
+        this.fieldVal17 = fieldVal17;
+    }
+    public String getFieldVal18() {
+        return fieldVal18;
+    }
+    public void setFieldVal18(String fieldVal18) {
+        this.fieldVal18 = fieldVal18;
+    }
+    public String getFieldVal19() {
+        return fieldVal19;
+    }
+    public void setFieldVal19(String fieldVal19) {
+        this.fieldVal19 = fieldVal19;
+    }
+    public String getFieldVal20() {
+        return fieldVal20;
+    }
+    public void setFieldVal20(String fieldVal20) {
+        this.fieldVal20 = fieldVal20;
+    }
+
+    public String getOptionField(int i ){
+        switch (i) {
+            case 0:
+                return field0;
+            case 1:
+                return field1;
+            case 2:
+                return field2;
+            case 3:
+                return field3;
+            case 4:
+                return field4;
+            case 5:
+                return field5;
+            case 6:
+                return field6;
+            case 7:
+                return field7;
+            case 8:
+                return field8;
+            case 9:
+                return field9;
+            case 10:
+                return field10;
+            case 11:
+                return field11;
+            case 12:
+                return field12;
+            case 13:
+                return field13;
+            case 14:
+                return field14;
+            case 15:
+                return field15;
+            case 16:
+                return field16;
+            case 17:
+                return field17;
+            case 18:
+                return field18;
+            case 19:
+                return field19;
+            case 20:
+                return field20;
+            default:
+                return null;
+        }
+    }
+
+    public String getOptionFieldVal(int i){
+        switch (i) {
+            case 0:
+                return fieldVal0;
+            case 1:
+                return fieldVal1;
+            case 2:
+                return fieldVal2;
+            case 3:
+                return fieldVal3;
+            case 4:
+                return fieldVal4;
+            case 5:
+                return fieldVal5;
+            case 6:
+                return fieldVal6;
+            case 7:
+                return fieldVal7;
+            case 8:
+                return fieldVal8;
+            case 9:
+                return fieldVal9;
+            case 10:
+                return fieldVal10;
+            case 11:
+                return fieldVal11;
+            case 12:
+                return fieldVal12;
+            case 13:
+                return fieldVal13;
+            case 14:
+                return fieldVal14;
+            case 15:
+                return fieldVal15;
+            case 16:
+                return fieldVal16;
+            case 17:
+                return fieldVal17;
+            case 18:
+                return fieldVal18;
+            case 19:
+                return fieldVal19;
+            case 20:
+                return fieldVal20;
+            default:
+                return null;
+        }
+    }
+
+    public void setOptionFieldVal(int i, String value){
+        switch (i) {
+            case 0:
+                fieldVal0 = value;
+                return;
+            case 1:
+                fieldVal1 = value;
+                return;
+            case 2:
+                fieldVal2 = value;
+                return;
+            case 3:
+                fieldVal3 = value;
+                return;
+            case 4:
+                fieldVal4 = value;
+                return;
+            case 5:
+                fieldVal5 = value;
+                return;
+            case 6:
+                fieldVal6 = value;
+                return;
+            case 7:
+                fieldVal7 = value;
+                return;
+            case 8:
+                fieldVal8 = value;
+                return;
+            case 9:
+                fieldVal9 = value;
+                return;
+            case 10:
+                fieldVal10 = value;
+                return;
+            case 11:
+                fieldVal11 = value;
+                return;
+            case 12:
+                fieldVal12 = value;
+                return;
+            case 13:
+                fieldVal13 = value;
+                return;
+            case 14:
+                fieldVal14 = value;
+                return;
+            case 15:
+                fieldVal15 = value;
+                return;
+            case 16:
+                fieldVal16 = value;
+                return;
+            case 17:
+                fieldVal17 = value;
+                return;
+            case 18:
+                fieldVal18 = value;
+                return;
+            case 19:
+                fieldVal19 = value;
+                return;
+            case 20:
+                fieldVal20 = value;
+                return;
+            default:
+                return;
+        }
+    }
+}
